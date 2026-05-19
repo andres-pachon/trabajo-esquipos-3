1cdd06001032a9802ad789845a10a6e52e101689
Ricy24 <discordandres20@gmail.com>
2026-05-19 16:59:49 -0500

    funcionalidad tmb

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,7 @@
-#aqui van todas las funciones 
+def calcular_tmb(peso:float, altura:float, edad:int, valor_genero:float) -> float:
+    valor_genero = 0
+    if valor_genero == 5:
+       valor_genero = -161
+
+    tmb=(10 * peso) + (6.25 * altura) - (5 * edad) + valor_genero
+    return tmb
--- vistaCalculadora.py
+++ vistaCalculadora.py
@@ -6,8 +6,12 @@ def InciarApp ():
     if opcion==1:
         print ("ejecutar calculadora de IMC", cal.calcularIMC)
     elif opcion == 2:
-        #aqui va a llamar
-        print()
+        peso = float(input("Ingrese su peso en kg: "))
+        altura = float(input("Ingrese su altura en cm: "))
+        edad = int(input("Ingrese su edad en años: "))
+        valor_genero = float(input("Ingrese su genero (5 para hombres, -161 para mujeres): "))
+        resultado_tmb = cal.calcular_tmb(peso, altura, edad, valor_genero)
+        print("Su TMB es:", resultado_tmb)
     elif opcion==3:
         #aqui va a llamar
         print()
