c9b367d38f376cac408dfe8d07fb73bffb9a20b8
Ricy24 <discordandres20@gmail.com>
2026-05-19 16:55:17 -0500

    plantilla inicial

---
+++ vistaCalculadora.py
@@ -0,0 +1,22 @@
+import calculadora as cal
+
+
+def InciarApp ():
+    opcion = int(input("Digite la opcion a calcular  1. Calcular IMC 2. Calcular TMB 3. Calculo de calorias en reposo 4. calcculo de calorias a adelgazar 5. porcentaje de grasa"))     
+    if opcion==1:
+        print ("ejecutar calculadora de IMC", cal.calcularIMC)
+    elif opcion == 2:
+        #aqui va a llamar
+        print()
+    elif opcion==3:
+        #aqui va a llamar
+        print()
+    elif opcion==4:
+        #aqui va a llamar
+        print()
+    elif opcion==5:
+        #aqui va a llamar
+        print()     
+    else:
+        print("Opcion no valida")
+InciarApp()
